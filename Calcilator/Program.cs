3fcfa41ccb2fd94eb8f88257a7544e0e1e44fb00
Zheni <jenijivkova07@gmail.com>
2025-12-16 10:32:18 +0200

    Initial commit

---
+++ Calcilator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcilator
+{
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Calcilator/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcilator
+{
+    internal class Test
+    {
+        [Test]
+        public void Add_TwoPlusTwo_ReturnsFour()
+        {
+            var calc = new Calculator();
+            Assert.That(calc.Add(2, 2), Is.EqualTo(4));
+        } 
+    }
+}
